7263b11c451f238207d811e24b0ca64f5b260644
Chiiip <pedro_pianista@hotmail.com>
2020-01-30 16:14:12 -0300

    adding empty page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { Router } from "@reach/router"
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 // import FormPage from "./pages/FormPage";
-
-const Home = lazy(() => import('./pages/Home'));
-const FormPage = lazy(() => import('./pages/FormPage'));
 
 function App() {
   return (
-    <Suspense fallback={<div>Carregando...</div>}>
     <Router>
         <Home path="/" />
         <FormPage path="form" />
     </Router>
-    </Suspense>
   );
 }
 
