2b2d40fb8e1082b929262ae0f1987d2e3fccf654
Chiiip <pedro_pianista@hotmail.com>
2020-01-30 16:14:47 -0300

    adding empty page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Router } from "@reach/router"
 import Home from "./pages/Home";
-// import FormPage from "./pages/FormPage";
 
 function App() {
   return (
     <Router>
         <Home path="/" />
-        <FormPage path="form" />
     </Router>
   );
 }
