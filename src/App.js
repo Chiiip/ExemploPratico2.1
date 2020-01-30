a63a3a0e0c40c4dc5a68c4b3123513d9ff8c6341
Chiiip <pedro_pianista@hotmail.com>
2020-01-30 16:15:54 -0300

    adding filled componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router } from "@reach/router"
 import Home from "./pages/Home";
+import FormPage from "./pages/FormPage";
 
 function App() {
   return (
     <Router>
         <Home path="/" />
+        <FormPage path="form" />
     </Router>
   );
 }
